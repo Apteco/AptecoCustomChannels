e211e85dc87bba65589bfaa299c69fedc8aa995a
Florian <florian.von.bracht@apteco.de>
2020-01-17 15:30:59 +0100

    Added more tls encryption options to flexmail

--- Flexmail/flexmail__15__getpreview.ps1
+++ Flexmail/flexmail__15__getpreview.ps1
@@ -77,9 +77,14 @@ $settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encodin
 # Allow only newer security protocols
 # hints: https://www.frankysweb.de/powershell-es-konnte-kein-geschuetzter-ssltls-kanal-erstellt-werden/
 if ( $settings.changeTLS ) {
-    $AllProtocols = [System.Net.SecurityProtocolType]'Tls12'
+    $AllProtocols = @(    
+        [System.Net.SecurityProtocolType]::Tls12
+        #[System.Net.SecurityProtocolType]::Tls13,
+        ,[System.Net.SecurityProtocolType]::Ssl3
+    )
     [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 }
+
 
 $logfile = $settings.logfile
 
