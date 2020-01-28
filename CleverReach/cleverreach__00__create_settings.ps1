51606461d1b1afb55f7f6a51e850817462d33ac2
Florian <florian.von.bracht@apteco.de>
2020-01-28 11:21:35 +0100

    Initial commit for CleverReach custom integration
    Support the functions "messages" and "preview"

---
+++ CleverReach/cleverreach__00__create_settings.ps1
@@ -0,0 +1,105 @@
+﻿
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+# Load scriptpath
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+} else {
+    $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+}
+
+Set-Location -Path $scriptPath
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+# General settings
+$functionsSubfolder = "functions"
+$settingsFilename = "settings.json"
+
+
+################################################
+#
+# FUNCTIONS
+#
+################################################
+
+Get-ChildItem -Path ".\$( $functionsSubfolder )" | ForEach {
+    . $_.FullName
+}
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+#-----------------------------------------------
+# LOGIN DATA
+#-----------------------------------------------
+
+$token = Read-Host -AsSecureString "Please enter the token for cleverreach"
+$tokenEncrypted = Get-PlaintextToSecure ((New-Object PSCredential "dummy",$token).GetNetworkCredential().Password)
+
+$login = @{
+    "accesstoken" = $tokenEncrypted
+}
+
+
+#-----------------------------------------------
+# PREVIEW SETTINGS
+#-----------------------------------------------
+
+$previewSettings = @{
+    "Type" = "Email" #Email|Sms
+    #"FromAddress"="info@apteco.de"
+    #"FromName"="Apteco"
+    "ReplyTo"="info@apteco.de"
+    #"Subject"="Test-Subject"
+}
+
+#-----------------------------------------------
+# ALL SETTINGS
+#-----------------------------------------------
+
+# TODO [ ] use url from PeopleStage Channel Editor Settings instead?
+
+$settings = @{
+    "base" = "https://rest.cleverreach.com/v3/"
+    "login" = $login
+    "rowsPerUpload" = 800
+    "changeTLS" = $true
+    "nameConcatChar" = " / "
+    "logfile" = "$( $scriptPath )\cr.log"
+    #"excludedAttributes" = @()
+    "previewSettings" = $previewSettings
+    "uploadsFolder" = "$( $scriptPath )\uploads\"
+}
+
+
+################################################
+#
+# PACK TOGETHER SETTINGS AND SAVE AS JSON
+#
+################################################
+
+# create json object
+$json = $settings | ConvertTo-Json -Depth 8 # -compress
+
+# print settings to console
+$json
+
+# save settings to file
+$json | Set-Content -path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8
+
+
+
---
+++ CleverReach/cleverreach__10__getmailings.ps1
@@ -0,0 +1,158 @@
+﻿################################################
+#
+# INPUT
+#
+################################################
+
+Param(
+    [hashtable] $params
+)
+
+#-----------------------------------------------
+# DEBUG SWITCH
+#-----------------------------------------------
+
+$debug = $true
+
+#-----------------------------------------------
+# INPUT PARAMETERS, IF DEBUG IS TRUE
+#-----------------------------------------------
+
+if ( $debug ) {
+    $params = [hashtable]@{
+	    Password= "def"
+	    scriptPath= "C:\FastStats\scripts\cleverreach"
+	    abc= "def"
+	    Username= "abc"
+    }
+}
+
+
+################################################
+#
+# NOTES
+#
+################################################
+
+<#
+
+https://rest.cleverreach.com/explorer/v3
+
+#>
+
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+if ( $debug ) {
+    # Load scriptpath
+    if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+        $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+    } else {
+        $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+    }
+} else {
+    $scriptPath = "$( $params.scriptPath )" 
+}
+Set-Location -Path $scriptPath
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+# General settings
+$functionsSubfolder = "functions"
+$settingsFilename = "settings.json"
+
+# Load settings
+$settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
+
+# Allow only newer security protocols
+# hints: https://www.frankysweb.de/powershell-es-konnte-kein-geschuetzter-ssltls-kanal-erstellt-werden/
+if ( $settings.changeTLS ) {
+    $AllProtocols = @(    
+        [System.Net.SecurityProtocolType]::Tls12
+        #[System.Net.SecurityProtocolType]::Tls13,
+        ,[System.Net.SecurityProtocolType]::Ssl3
+    )
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+}
+
+# TODO [ ] change campaign type
+
+# more settings
+$logfile = $settings.logfile
+
+
+################################################
+#
+# FUNCTIONS
+#
+################################################
+
+Get-ChildItem -Path ".\$( $functionsSubfolder )" | ForEach {
+    . $_.FullName
+}
+
+################################################
+#
+# LOG INPUT PARAMETERS
+#
+################################################
+
+"$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`t----------------------------------------------------" >> $logfile
+"$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`tGETMAILINGS" >> $logfile
+"$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`tGot a file with these arguments: $( [Environment]::GetCommandLineArgs() )" >> $logfile
+$params.Keys | ForEach {
+    $param = $_
+    "$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`t $( $param ): $( $params[$param] )" >> $logfile
+}
+
+
+################################################
+#
+# PROGRAM
+#
+################################################
+
+#-----------------------------------------------
+# AUTHENTICATION
+#-----------------------------------------------
+
+$auth = "Bearer $( Get-SecureToPlaintext -String $settings.login.accesstoken )"
+$header = @{
+    "Authorization" = $auth
+}
+
+$apiRoot = $settings.base
+
+#-----------------------------------------------
+# GET MAILINGS / CAMPAIGNS
+#-----------------------------------------------
+
+$object = "mailings"
+$endpoint = "$( $apiRoot )$( $object ).json"
+$res = Invoke-RestMethod -Method Get -Uri $endpoint -Headers $header
+
+
+#-----------------------------------------------
+# GET MAILINGS / CAMPAIGNS DETAILS
+#-----------------------------------------------
+
+$messages = $res.draft | Select @{name="id";expression={ $_.id }}, @{name="name";expression={ "$( $_.id )$( $settings.nameConcatChar )$( $_.name )" }}
+
+
+################################################
+#
+# RETURN
+#
+################################################
+
+# real messages
+return $messages
+
