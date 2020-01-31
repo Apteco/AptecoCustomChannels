ce972c510e281da899f0809fc9342e35767180fd
Florian <florian.von.bracht@apteco.de>
2020-01-31 14:14:53 +0100

    Create cleverreach__05__test.ps1
    Outline for a test connection script

---
+++ CleverReach/cleverreach__05__test.ps1
@@ -0,0 +1,148 @@
+################################################
+#
+# INPUT
+#
+################################################
+
+Param(
+    [hashtable] $params
+)
+
+#-----------------------------------------------
+# DEBUG SWITCH
+#-----------------------------------------------
+
+$debug = $true
+
+#-----------------------------------------------
+# INPUT PARAMETERS, IF DEBUG IS TRUE
+#-----------------------------------------------
+
+# TODO [ ] check input parameter
+
+if ( $debug ) {
+    $params = [hashtable]@{
+	    Password= "def"
+	    scriptPath= "C:\FastStats\scripts\cleverreach"
+	    abc= "def"
+	    Username= "abc"
+    }
+}
+
+
+################################################
+#
+# NOTES
+#
+################################################
+
+<#
+
+https://rest.cleverreach.com/explorer/v3
+
+#>
+
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+if ( $debug ) {
+    # Load scriptpath
+    if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+        $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+    } else {
+        $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+    }
+} else {
+    $scriptPath = "$( $params.scriptPath )" 
+}
+Set-Location -Path $scriptPath
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+# General settings
+$functionsSubfolder = "functions"
+$settingsFilename = "settings.json"
+
+# Load settings
+$settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
+
+# Allow only newer security protocols
+# hints: https://www.frankysweb.de/powershell-es-konnte-kein-geschuetzter-ssltls-kanal-erstellt-werden/
+if ( $settings.changeTLS ) {
+    $AllProtocols = @(    
+        [System.Net.SecurityProtocolType]::Tls12
+        #[System.Net.SecurityProtocolType]::Tls13,
+        ,[System.Net.SecurityProtocolType]::Ssl3
+    )
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+}
+
+# more settings
+$logfile = $settings.logfile
+$contentType = $settings.contentType
+
+
+################################################
+#
+# FUNCTIONS
+#
+################################################
+
+Get-ChildItem -Path ".\$( $functionsSubfolder )" | ForEach {
+    . $_.FullName
+}
+
+################################################
+#
+# LOG INPUT PARAMETERS
+#
+################################################
+
+"$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`t----------------------------------------------------" >> $logfile
+"$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`tTEST" >> $logfile
+"$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`tGot a file with these arguments: $( [Environment]::GetCommandLineArgs() )" >> $logfile
+$params.Keys | ForEach {
+    $param = $_
+    "$( [datetime]::Now.ToString("yyyyMMddHHmmss") )`t $( $param ): $( $params[$param] )" >> $logfile
+}
+
+
+################################################
+#
+# PROGRAM
+#
+################################################
+
+#-----------------------------------------------
+# AUTHENTICATION
+#-----------------------------------------------
+
+$auth = "Bearer $( Get-SecureToPlaintext -String $settings.login.accesstoken )"
+$header = @{
+    "Authorization" = $auth
+}
+
+$apiRoot = $settings.base
+
+#-----------------------------------------------
+# GET MAILINGS / CAMPAIGNS
+#-----------------------------------------------
+
+# TODO [ ] check for the right endpoint to test connection
+
+<#
+$object = "mailings"
+$endpoint = "$( $apiRoot )$( $object ).json"
+$res = Invoke-RestMethod -Method Get -Uri $endpoint -Headers $header -ContentType $contentType
+#>
+
+
+# TODO [ ] Is there something expected to return? Something like true or false?
