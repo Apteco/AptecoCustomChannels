54d667a4f825855dcb29bf618273a87dab29d386
Florian <florian.von.bracht@apteco.de>
2020-02-04 12:54:30 +0100

    Added more log messages

--- EpiServerCampaign/SmartCampaigns/epi__20__upload_list.ps1
+++ EpiServerCampaign/SmartCampaigns/epi__20__upload_list.ps1
@@ -149,6 +149,7 @@ $params.Keys | ForEach {
 #-----------------------------------------------
 
 if ( !(Test-Path -Path $uploadsFolder) ) {
+    Write-Log -message "Upload $( $uploadsFolder ) does not exist. Creating the folder now!"
     New-Item -Path "$( $uploadsFolder )" -ItemType Directory
 }
 
@@ -167,10 +168,14 @@ if ( !(Test-Path -Path $uploadsFolder) ) {
 $messageName = $params.MessageName
 $smartCampaignID = $messageName -split $settings.nameConcatChar | select -First 1
 
+Write-Log -message "Recognised the smart campaign id $( $smartCampaignID )"
+
 
 #-----------------------------------------------
 # SESSION
 #-----------------------------------------------
+
+Write-Log -message "Opening a new session in EpiServer valid for $( $settings.ttl )"
 
 Get-EpiSession
 
@@ -179,56 +184,85 @@ Get-EpiSession
 # GET MAILINGS / CAMPAIGNS
 #-----------------------------------------------
 
+Write-Log -message "Checking the campaign id"
+
 $campaigns = Get-EpiCampaigns -campaignType $campaignType
 
 if ($campaigns -notcontains $smartCampaignID) {
     Write-Log -message "No valid campaign/mailing ID"  
     throw [System.IO.InvalidDataException] "No valid campaign/mailing ID"  
-} 
+} else {
+    Write-Log -message "Campaign ID seems to be valid"
+}
+
 
 
 #-----------------------------------------------
 # ATTRIBUTES
 #-----------------------------------------------
 
+Write-Log -message "Checking attributes for $( $settings.masterListId )"
+
 $listAttributesRaw = Invoke-Epi -webservice "RecipientList" -method "getAttributeNames" -param @(@{value=$settings.masterListId;datatype="long"},@{value="en";datatype="String"}) -useSessionId $true
+
+Write-Log -message "Got back $( $listAttributesRaw )"
+
 #$listAttributes = $listAttributesRaw | where { $_ -notin $excludedAttributes }
 $listAttributes = [array]$params.UrnFieldName + [array]( $listAttributesRaw | where  { $_ -notin $excludedAttributes } )
+
+Write-Log -message "Filtered the attributes. Those fields are left: $( $listAttributes )"
 
 
 #-----------------------------------------------
 # DATA MAPPING
 #-----------------------------------------------
 
-#"$( [datetime]::UtcNow.ToString("yyyyMMddHHmmss") )`tStart to create a new file"
 $fileItem = Get-Item -Path $params.Path
 
-# TODO [ ] put in the measure command from the marketing automation module
-$exportId = Split-File -inputPath $fileItem.FullName -header $true -writeHeader $true -inputDelimiter "`t" -outputDelimiter "`t" -outputColumns $listAttributes -writeCount $maxWriteCount -outputDoubleQuotes $false -outputPath $uploadsFolder
+Write-Log -message "Loading the file $( $fileItem.FullName )"
 
-#"$( [datetime]::UtcNow.ToString("yyyyMMddHHmmss") )`tDone with export id $( $exportId )!"
+$exportId = ""
+$t = Measure-Command {
+
+    # TODO [x] put in the measure command from the marketing automation module
+    $exportId = Split-File -inputPath $fileItem.FullName -header $true -writeHeader $true -inputDelimiter "`t" -outputDelimiter "`t" -outputColumns $listAttributes -writeCount $maxWriteCount -outputDoubleQuotes $false -outputPath $uploadsFolder
+
+}
+
+Write-Log -message "Done with export id $( $exportId ) in $( $t.Seconds ) seconds"
 
 
 #-----------------------------------------------
 # NEW WAVE
 #-----------------------------------------------
 
+Write-Log -message "Creating a new wave for $( $smartCampaignID )"
+
 $waveId = Invoke-Epi -webservice "ClosedLoop" -method "prepareNewWave" -param @($smartCampaignID) -useSessionId $true
 
+Write-Log -message "Got back wave id $( $waveId )"
 
 #-----------------------------------------------
 # IMPORT RECIPIENTS
 #-----------------------------------------------
 
+Write-Log -message "Beginning to load the splitted file now"
+
+# Loading all splitted and filtered files
 $importResults = 0
 Get-ChildItem -Path "$( $uploadsFolder )\$( $exportId )" | ForEach {
 
+    # Creating the file object
     $f = $_
     
+    Write-Log -message "Loading $( $f.FullName )"
+
+    # Loading the part file
     $csv = Get-Content -Path "$( $f.FullName )" -Encoding UTF8 
     
+    # Creating the array without the header
     $valArr = @()
-    $csv | select -skip 1 | ForEach {
+    $csv | Select-Object -skip 1 | ForEach-Object {
         $line = $_
         $valArr += ,( $line -split "`t" )
         $importResults += 1
@@ -236,6 +270,9 @@ Get-ChildItem -Path "$( $uploadsFolder )\$( $exportId )" | ForEach {
 
     # Change the urn field name for the upload list
     $listAttributes = $listAttributes -replace $params.UrnFieldName, $urnFieldName
+
+    Write-Log -message "Changed the urn field name from $( $params.UrnFieldName ) to $urnFieldName"
+    Write-Log -message "Those fields are left to upload: $( $listAttributes )"
 
     # Bring all parameter together for the upload
     $paramsEpi = @(
@@ -247,12 +284,27 @@ Get-ChildItem -Path "$( $uploadsFolder )\$( $exportId )" | ForEach {
     # Upload data
     Invoke-Epi -webservice "ClosedLoop" -method "importRecipients" -param $paramsEpi -useSessionId $true
 
+    Write-Log -message "File part upload done. Total $( $importResults )"
+
 } 
+
+Write-Log -message "Whole upload done. Uploaded $( $importResults )"
 
 # Schedule the mailing
 # TODO [ ] remove this after the test with release 2019-Q4
 Invoke-Epi -webservice "ClosedLoop" -method "importFinishedAndScheduleMailing" -param @(@{value=$waveId;datatype="long"}) -useSessionId $true
 
+Write-Log -message "Triggered the wave now and created a mailing."
+switch ( $settings.syncType ) {
+
+    "async" {
+        Write-Log -message "async upload -> The upload is done now and the mailing id will be enriched later"
+    }
+
+    "sync" {
+        Write-Log -message "synced upload -> loop until mailing id is send back"
+    }
+}
 
 ################################################
 #
@@ -260,13 +312,15 @@ Invoke-Epi -webservice "ClosedLoop" -method "importFinishedAndScheduleMailing" -
 #
 ################################################
 
-# TODO [ ] Implement Write-Host for messages in the system health monitor and in the deliveryjobsummary table in the ws-database 
+# TODO [x] Implement Write-Host for messages in the system health monitor and in the deliveryjobsummary table in the ws-database 
 
 # count the number of successful upload rows
 $recipients = $importResults
 
 # put in the source id as the listname
 $transactionId = $waveId
+
+Write-Host "Uploaded $( $recipients ) records with wave id $( $transactionId )"
 
 # return object
 $return = [Hashtable]@{
