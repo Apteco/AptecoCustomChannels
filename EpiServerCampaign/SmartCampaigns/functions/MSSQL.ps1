536fcdd47047dcad77f58c946e6123a6f9fc8a7c
Florian <florian.von.bracht@apteco.de>
2020-02-04 23:28:00 +0100

    Update MSSQL.ps1
    Added timeout, a scalar command and outlines for bulkcopy

--- EpiServerCampaign/SmartCampaigns/functions/MSSQL.ps1
+++ EpiServerCampaign/SmartCampaigns/functions/MSSQL.ps1
@@ -15,6 +15,7 @@ Function Query-SQLServer {
         # execute command
         $mssqlCommand = $mssqlConnection.CreateCommand()
         $mssqlCommand.CommandText = $query
+        $mssqlCommand.CommandTimeout = $settings.commandTimeout
         $mssqlResult = $mssqlCommand.ExecuteReader()
         
         # load data
@@ -56,6 +57,7 @@ Function NonQuery-SQLServer {
         # execute command
         $mssqlCommand = $mssqlConnection.CreateCommand()
         $mssqlCommand.CommandText = $command
+        $mssqlCommand.CommandTimeout = $settings.commandTimeout
         $result = $mssqlCommand.ExecuteNonQuery()
         
         # return result datatable
@@ -75,3 +77,69 @@ Function NonQuery-SQLServer {
     }
 
 }
+
+
+Function NonQueryScalar-SQLServer {
+
+    param(
+         [Parameter(Mandatory=$true)][string]$connectionString 
+        ,[Parameter(Mandatory=$true)][string]$command 
+    )
+
+    try {
+
+        # build connection
+        $mssqlConnection = New-Object "System.Data.SqlClient.SqlConnection"
+        $mssqlConnection.ConnectionString = $connectionString 
+        $mssqlConnection.Open()
+        
+        # execute command
+        $mssqlCommand = $mssqlConnection.CreateCommand()
+        $mssqlCommand.CommandText = $command
+        $mssqlCommand.CommandTimeout = $settings.commandTimeout
+        $result = $mssqlCommand.ExecuteScalar()
+        
+        # return result datatable
+        return $result
+
+    } catch [System.Exception] {
+
+        $errText = $_.Exception
+        $errText | Write-Output
+        #Write-Log -message "Error: $( $errText )"
+
+    } finally {
+        
+        # close connection
+        $mssqlConnection.Close()
+
+    }
+
+}
+
+<#
+
+Notes for bulk copy
+
+    try {
+
+    $sqlBulkCopy = New-Object -TypeName System.Data.SqlClient.SqlBulkCopy($mssqlConnectionString, [System.Data.SqlClient.SqlBulkCopyOptions]::KeepIdentity)
+    #$sqlBulkCopy.EnableStreaming = $true
+    $sqlBulkCopy.DestinationTableName = $bulkDestination
+    $sqlBulkCopy.BatchSize = $settings.bulkBatchsize
+    $sqlBulkCopy.BulkCopyTimeout = $settings.bulkTimeout
+    $bulkResult = $sqlBulkCopy.WriteToServer($campaignMetadata)
+
+    } catch [System.Exception] {
+
+        $errText = $_.Exception
+        $errText | Write-Output
+        Write-Log -message "Error: $( $errText )"
+
+    } finally {
+
+        $sqlBulkCopy.Close()
+
+    }
+
+#>
