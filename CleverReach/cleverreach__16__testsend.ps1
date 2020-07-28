87528b88bc35150b047f8ab30b67ae821b168409
Florian <florian.von.bracht@apteco.de>
2020-07-28 21:05:17 +0200

    removed until developed

--- CleverReach/cleverreach__16__testsend.ps1
+++
@@ -1,7 +0,0 @@
-# send preview
-$preview = [pscustomobject]@{
-    receivers = @("florian.von.bracht@apteco.de")
-    previewText=" - have a look"
-}
-$previewJson = $preview | ConvertTo-Json -Depth 8 -Compress
-$previewMailing = Invoke-RestMethod -Method POST -Uri "$( $mailingsUrl )/$( $copiedMailing.id )/sendpreview" -Headers $header -Verbose -Body $previewJson -ContentType $contentType
