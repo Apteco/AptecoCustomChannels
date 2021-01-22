a1b46e7c37d42bd0d17a94ccc8924fcc10170808
FlorianSf <florian.von.bracht@apteco.de>
2021-01-22 14:35:32 +0100

    Create elaine__16__testsend.ps1
    Outlines for a test send function

---
+++ ELAINE/Transactional/elaine__16__testsend.ps1
@@ -0,0 +1,15 @@
+
+#-----------------------------------------------
+# TEST SEND
+#-----------------------------------------------
+<#
+Upload an array in the api call and send email directly
+#>
+
+$function = "api_mailingTestsend"
+$jsonInput = @(
+    ""      # int $nl_id
+    ""      # int $ev_id
+) 
+
+$testsend = Invoke-ELAINE -function "api_mailingTestsend" -method Post -parameters $jsonInput
