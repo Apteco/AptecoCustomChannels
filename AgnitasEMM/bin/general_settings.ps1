06def99b91c4ec7edc64414ec6808e32e70a0c7d
Florian von Bracht <github@ritterstation.de>
2021-10-04 11:10:17 +0200

    Delete AgnitasEMM directory
    Moving these files to another directory

--- AgnitasEMM/bin/general_settings.ps1
+++
@@ -1,6 +0,0 @@
-
-$functionsSubfolder = "functions"
-$libSubfolder = "lib"
-$settingsFilename = "settings.json"
-$processId = [guid]::NewGuid()
-$timestamp = [datetime]::Now
--- AgnitasEMM/bin/load_settings.ps1
+++
@@ -1,3 +0,0 @@
-# Load settings
-$settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
-
